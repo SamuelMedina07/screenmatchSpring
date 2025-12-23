3c0b1347223f95a3b27b008cf5be0537497eb6e8
SamuelMedina07 <semedina@uth.hn>
2025-12-22 20:00:48 -0600

    Add servicios para consumo de api, interfaz para crear clases y metodos <T>, modelado de datos de tempradas x episodios

--- src/main/java/com/aluraweb/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluraweb/screenmatch/ScreenmatchApplication.java
@@ -1,8 +1,16 @@
 package com.aluraweb.screenmatch;
 
+import com.aluraweb.screenmatch.model.DatosEpisodio;
+import com.aluraweb.screenmatch.model.DatosSerie;
+import com.aluraweb.screenmatch.model.DatosTemporada;
+import com.aluraweb.screenmatch.service.ConsumoAPI;
+import com.aluraweb.screenmatch.service.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -13,6 +21,24 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Hola Mundo desde Spring");
+       // System.out.println("Hola Mundo desde Spring");
+        var consumoAPi = new ConsumoAPI();
+        var json = consumoAPi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&apikey=2b83c21c");
+        System.out.println(json);
+        ConvierteDatos conversor = new ConvierteDatos();
+        var datos = conversor.obtenerDatos(json, DatosSerie.class);
+        System.out.println(datos);
+
+        var json2 = consumoAPi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season=1&episode=1&apikey=2b83c21c");
+        var datos2 = conversor.obtenerDatos(json2, DatosEpisodio.class);
+        System.out.println(datos2);
+
+        List<DatosTemporada> temporadas = new ArrayList<>();
+        for (int i = 1; i < datos.totalDeTemporadas(); i++) {
+            var json3 = consumoAPi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season="+i+"&apikey=2b83c21c");
+            var datosTemporadas = conversor.obtenerDatos(json3, DatosTemporada.class);
+            temporadas.add(datosTemporadas);
+        }
+        temporadas.forEach(System.out::println);
     }
 }
---
+++ src/main/java/com/aluraweb/screenmatch/model/DatosSerie.java
@@ -0,0 +1,14 @@
+package com.aluraweb.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//ponemos esta linea para ignorar campos del json que no queremos ejemplo Year
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosSerie(
+
+        @JsonAlias("Title") String titulo,
+        @JsonAlias("totalSeasons") Integer totalDeTemporadas,
+        @JsonAlias("imdbRating") String evaluacion
+
+) {
+}
